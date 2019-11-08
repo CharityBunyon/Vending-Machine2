a88e769109bf464a37425052757aad8213dd58b0
Charity Bunyon <cseymou5@gmail.com>
2019-11-07 18:18:21 -0600

    components

---
+++ src/javascripts/helpers/data/authData.js
@@ -0,0 +1,28 @@
+import $ from 'jquery';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import stocker from '../../components/Stocker/stocker';
+
+const authDiv = $('#auth');
+const stockDiv = $('#stock');
+const logoutNavbar = $('#navbar-button-logout');
+
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // someone is logged in - we should NOT see auth component
+      stockDiv.removeClass('hide');
+      logoutNavbar.removeClass('hide');
+      authDiv.addClass('hide');
+      stocker.buildTheStocker(user.uid);
+    } else {
+      // nobody logged in SHOW auth component
+      stockDiv.addClass('hide');
+      logoutNavbar.addClass('hide');
+      authDiv.removeClass('hide');
+    }
+  });
+};
+
+export default { checkLoginStatus };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,21 @@
+import firebase from 'firebase';
 import 'bootstrap';
+
+import auth from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/MyNavbar/myNavbar';
+import machine from './components/Machine/machine';
+
+import apiKeys from './helpers/apiKeys.json';
 
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  myNavbar.logoutEvent();
+  machine.buildTheMachine();
+};
+
+init();
